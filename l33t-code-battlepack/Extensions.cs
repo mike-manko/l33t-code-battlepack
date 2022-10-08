7040f75221ed1e0953a6ef21b79ccfe26d9ed1aa
Michal Manko <michalmanko@gmail.com>
2022-10-09 01:49:52 +0200

    - Created project

---
+++ l33t-code-battlepack/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace l33t_code_battlepack
+{
+    public static class Extensions
+    {
+        public static IEnumerable<IEnumerable<T>> GetCombinationsOfLength<T>(this IEnumerable<T> elements, int k)
+        {
+            return k == 0 ? new[] { Array.Empty<T>() } :
+                elements.SelectMany((e, i) => elements.Skip(i + 1).GetCombinationsOfLength(k - 1).Select(c => (new[] { e }).Concat(c)));
+        }
+    }
+}
---
+++ l33t-code-battlepack/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace l33t_code_battlepack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
